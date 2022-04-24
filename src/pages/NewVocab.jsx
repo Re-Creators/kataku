aff06bd7044c6c703dcc5a4cb2b651df24ea302f
= <richardoehbet221@gmail.com>
2022-04-24 16:32:40 +0700

    remove unused imnport

--- src/pages/NewVocab.jsx
+++ src/pages/NewVocab.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react"
 import Notification from "../components/Notification"
-import Spinner from "../components/Spinner"
 import { CSSTransition } from "react-transition-group"
 import { useDispatch, useSelector } from "react-redux"
 import { updateUserLanguages, userSelector } from "../features/user/userSlice"

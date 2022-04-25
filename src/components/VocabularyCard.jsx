c0b7b997889c32fcc37ef55dd8d4d7b09bda94df
= <richardoehbet221@gmail.com>
2022-04-25 10:45:34 +0700

    style : fix oveflow text content

--- src/components/VocabularyCard.jsx
+++ src/components/VocabularyCard.jsx
@@ -1,12 +1,12 @@
-import { useContext } from "react";
-import { BsCalendar3 } from "react-icons/bs";
-import VocabularyContext from "../context/VocabularyContext";
+import { useContext } from "react"
+import { BsCalendar3 } from "react-icons/bs"
+import VocabularyContext from "../context/VocabularyContext"
 
-import useBadge from "../hooks/useBadge";
+import useBadge from "../hooks/useBadge"
 
 function VocabularyCard({ vocabulary }) {
-  const { showBadge, badgeText } = useBadge(vocabulary);
-  const { showModal } = useContext(VocabularyContext);
+  const { showBadge, badgeText } = useBadge(vocabulary)
+  const { showModal } = useContext(VocabularyContext)
 
   return (
     <div
@@ -15,11 +15,7 @@ function VocabularyCard({ vocabulary }) {
     >
       {showBadge && (
         <div className="absolute top-1 -right-3 w-20 h-14">
-          <img
-            src={`/images/${badgeText}-badge.svg`}
-            alt=""
-            className="w-full h-full"
-          />
+          <img src={`/images/${badgeText}-badge.svg`} alt="" className="w-full h-full" />
 
           <p className="absolute w-full text-center top-1/2 -translate-y-1/2 text-white text-sm">
             {badgeText}
@@ -31,16 +27,16 @@ function VocabularyCard({ vocabulary }) {
         <BsCalendar3 fontSize={14} />
         <span className="text-xs">{vocabulary.createdAt.substr(0, 10)}</span>
       </div>
-      <div className="flex items-center justify-center mt-8 flex-col">
-        <div className="text-2xl lg:text-2xl xl:text-4xl text-primary font-bold capitalize">
+      <div className="flex items-center justify-center mt-5 flex-col">
+        <div className="w-full py-2 line-clamp-1 text-center text-2xl lg:text-2xl xl:text-4xl text-primary font-bold capitalize">
           {vocabulary.vocab}
         </div>
-        <div className="text-lg lg:text-xl xl:text-2xl  font-bold mt-4 capitalize">
+        <div className="text-lg lg:text-xl xl:text-2xl  font-bold mt-4 capitalize line-clamp-2">
           {vocabulary.translate}
         </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default VocabularyCard;
+export default VocabularyCard
